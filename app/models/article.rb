f3281863bae55f09e538c5123455d67d76ac3047
Svprak <svprak@gmail.com>
2016-05-02 10:35:04 +0700

    Create Model and migrate db and add validation

---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ActiveRecord::Base
+  validates :title, presence: true, length: {minimum: 5, maximum:50}
+  validates :description, presence:true, length: {minimum:20, maximum:500}
+end
