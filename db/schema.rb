41bda12c4bb437f119b1c5e094f91d98b0f2b7f6
Svprak <svprak@gmail.com>
2016-05-03 13:58:02 +0700

    Association and debger view

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503055930) do
+ActiveRecord::Schema.define(version: 20160503063621) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
