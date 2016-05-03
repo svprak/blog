703abb6ba9e30a37d54843eeea85d4b1c7e2ba60
Svprak <svprak@gmail.com>
2016-05-03 17:45:07 +0700

    complete pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :show, :update]
   def index
-    @users = User.all
+    #@users = User.all
+    @users =User.paginate(page:params[:page], per_page: 5)
   end
   def new
     @user = User.new
@@ -15,10 +17,10 @@ class UsersController < ApplicationController
     end
   end
   def edit
-    @user = User.find(params[:id])
+
   end
   def update
-    @user =User.find(params[:id])
+
     if @user.update(user_params)
       flash[:success] = "Update successfully"
       redirect_to articles_path
@@ -27,12 +29,15 @@ class UsersController < ApplicationController
     end
   end
   def show
-    @user = User.find(params[:id])
+    @user_articles = @user.articles.paginate(page:params[:page], per_page:3)
   end
 
 
 
   private
+    def set_user
+      @user = User.find(params[:id])
+    end
     def user_params
       params.require(:user).permit(:username, :email, :password)
     end
