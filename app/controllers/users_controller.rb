50c2fccf4b34b2cdc6312aefd718a2c61bb81871
Svprak <svprak@gmail.com>
2016-05-04 16:30:55 +0700

    adjustment to the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,17 @@ class UsersController < ApplicationController
   end
   def new
     @user = User.new
+    if session[:user_id] != nil
+      flash[:danger] = "Please SIGN OUT before you can sign up."
+      redirect_to root_path
+    end
   end
   def create
     @user = User.new(user_params)
     if @user.save
       session[:user_id]= @user.id
       flash[:success] = "Welcome to the blog #{@user.username}"
-      redirect_to articles_path(@user)
+      redirect_to articles_path
     else
       render 'new'
     end
@@ -23,7 +27,6 @@ class UsersController < ApplicationController
 
   end
   def update
-
     if @user.update(user_params)
       flash[:success] = "Update successfully"
       redirect_to articles_path
