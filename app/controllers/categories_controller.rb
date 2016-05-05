0839c78b93a4639862af7d3dcf1197f584a6c868
Svprak <svprak@gmail.com>
2016-05-05 15:37:57 +0700

    restrick from creating category if not admin

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except:[:index, :show]
   def index
     #@categories =Category.all
     @categories = Category.paginate(page:params[:page], per_page: 5)
@@ -23,5 +24,14 @@ class CategoriesController < ApplicationController
   private
     def category_params
       params.require(:category).permit(:name)
+    end
+
+    def require_admin
+      if !logged_in?  || (logged_in? and !current_user.admin?)
+        flash[:danger] ="Allow this only for admin"
+        redirect_to categories_path
+      else
+
+      end
     end
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -2,18 +2,26 @@ require 'test_helper'
 class CategoriesControllerTest < ActionController::TestCase
   def setup
     @category =Category.create(name:'sports')
+    @user = User.create(username:"Jamies", email:"jamies@gmail.com", password:"123", admin:true)
   end
   test "Should get categories index" do
     get :index
     assert_response :success
   end
   test "should get new" do
+    session[:user_id] = @user.id
     get :new
     assert_response :success
   end
   test "show" do
     get(:show, {'id' => @category.id})
     assert_response :success
+  end
+  test 'should not allow create if not admin' do
+    assert_no_difference 'Category.count' do
+      post :create, category: {name:'sports'}
+    end
+    assert_redirected_to categories_path
   end
 
 end
